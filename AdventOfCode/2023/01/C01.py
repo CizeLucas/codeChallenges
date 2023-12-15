ace8415ba0ce9b99c76e59514a03c483f7c89d1f
CizeLPC <cizelucas@gmail.com>
2023-12-15 10:53:02 -0300

    started adding the spelled numbers checking

--- AdventOfCode/2023/01/C01.py
+++ AdventOfCode/2023/01/C01.py
@@ -1,16 +1,25 @@
 #  --- Day 1: Trebuchet?! ---
 
+numbers = (['o', 't', 'f', 's', 'e', 'n'], ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"], [1,2,3,4,5,6,7,8,9])
+
 def get_calibration_numbers(string):
     # first_number -> stores the first number it finds in the line
     # second_number -> will store all the other numbers found while iterating over the line 
     firstNumber = -1
     secondNumber = -1
     for i in range(len(string)):
-        if string[i].isdigit():
+        if string[i] in numbers[0]:
+            spelled_number = get_spelled_numbers(string[i:])
             if firstNumber!=(-1):
-                secondNumber = int(string[i])
+                secondNumber = spelled_number
             else:
-                firstNumber = int(string[i])
+                firstNumber = spelled_number
+        else: 
+            if string[i].isdigit():
+                if firstNumber!=(-1):
+                    secondNumber = int(string[i])
+                else:
+                    firstNumber = int(string[i])
 
             if secondNumber==(-1):
                 secondNumber = firstNumber
@@ -20,27 +29,20 @@ def get_calibration_numbers(string):
     print(f"{string} -> {finalResult}") 
 
     return finalResult
+# o, t, f, s, e, n
+def get_spelled_numbers(string):
+    for index in range(len(numbers[1])):
+        if(numbers[1][index] in string):
+            return numbers[2][index]
 
+    return -1
 
 directory = "C:\\dev\\codeChallenges\\AdventOfCode\\2023\\01\\"
+
+file = open(directory+"input.txt", 'r')
+print(get_calibration_numbers(file.readline()))
+
 """
-file = open(directory+'input.txt', "r")
-
-sum_of_calib_values=0
-counter = 0
-
-for line in file:
-    print(f"{counter}) ")
-    sum_of_calib_values += get_calibration_numbers(file.readline()[:-1])
-    counter+=1
-    # string[:-1] excludes the last character of the string (excludes '\n')
-    
-
-print(f"The sum of all the calibration values is: {sum_of_calib_values}")
-
-file.close()
-"""
-
 with open(directory+"input.txt", 'r') as file:
     counter=1
     sum_of_calib_values=0
@@ -52,4 +54,5 @@ with open(directory+"input.txt", 'r') as file:
         # string[:-1] excludes the last character of the string (excludes '\n')
 
 print(f"The sum of all the calibration values is: {sum_of_calib_values}")
-#SPOILER: the answer is 54304
+#SPOILER: the first answer is 54304
+"""
